5c141393dc9b523e4340a77afa6d93c4e52dd69b
U-BSD\419097 <419097@SHS-9YBF6X3.beaverton.k12.or.us>
2024-10-01 09:33:32 -0700

    win functions

--- TTT.cpp
+++ TTT.cpp
@@ -10,27 +10,42 @@
 
 using namespace std;
 
-void printBoard();
-bool isValid(char row, char col);
-void placePlayer(char player, char row, char col);
-void takeTurn(char player);
+void printBoard(char board[3][3]);
+bool isValid(int row, int col, char board[3][3]);
+void placePlayer(char player, int row, int col, char board[3][3]);
+void takeTurn(char player, char board[3][3]);
 
-char row;
-char col;
-char player;
-char board[3][3] = {
-    { '-', '-', '-', },
-    { '-', '-', '-', },
-    { '-', '-', '-', }
-};
+bool checkRowWin(char player, char board[3][3]);
+bool checkColWin(char player, char board[3][3]);
+bool checkDiagWin(char player, char board[3][3]);
+bool checkWin(char player, char board[3][3]);
+bool checkTie(char board[3][3]);
+
 
 int main() {
   char player = 'X';
-  printBoard();
-  takeTurn(player);
+  char board[3][3] = {
+    { '-', '-', '-', },
+    { '-', '-', '-', },
+    { '-', '-', '-', }
+  };
+  printBoard(board);
+
+  while (checkWin(player, board) == false && checkTie(board) == false) {
+    takeTurn(player, board);
+    if (player == 'X' && checkWin(player, board) == false) {
+      player = 'O';
+    }
+    else if (checkWin(player, board) == false) {
+      player = 'X';
+    }
+    if (checkTie(board) == true) {
+      cout << "Tie!" << endl;
+    }
+  }
 }
 
-void printBoard() {
+void printBoard(char board[3][3]) {
   cout << "Welcome to Tic Tac Toe!" << endl;
   cout << "\n" << endl;
   cout << "\t0\t1\t2" << endl;
@@ -42,7 +57,7 @@ void printBoard() {
   cout << "2\t" << board[2][0] << "\t" << board[2][1] << "\t" << board[2][2] << endl;
 }
 
-bool isValid(char row, char col) {
+bool isValid(int row, int col, char board[3][3]) {
   cout << row << col << endl;
   if (board[row][col] == '-') { //heeeeeeeeeeeeeeeeerrrrrrrreeeeee
     return true;
@@ -53,24 +68,107 @@ bool isValid(char row, char col) {
   }
 }
 
-void placePlayer(char player, char row, char col) {
-  board[int(row)][int(col)] = player;
-  printBoard();
+void placePlayer(char player, int row, int col, char board[3][3]) {
+  board[row][col] = player;
+  printBoard(board);
 }
 
-void takeTurn(char player) {
+void takeTurn(char player, char board[3][3]) {
+  int row;
+  int col;
   cout << player << "'s Turn!" << endl;
   cout << "Enter a row!" << endl;
   cin >> row;
   cout << "Enter a column!" << endl;
   cin >> col;
-  if (isValid(row, col) == true) {
-    placePlayer(player, row, col);
+  if (isValid(row, col, board) == true) {
+    placePlayer(player, row, col, board);
   }
   else {
     cout << "Please enter a valid move." << endl;
-    takeTurn(player);
+    takeTurn(player, board);
   }
 }
 
+bool checkRowWin(char player, char board[3][3]) {
+  int win = 0;
+  for (int a = 0; a < 3; a++) {
+    win = 0;
+    for (int b = 0; b < 3; b++) {
+      if (board[b][a] != player) {
+	win++;
+      }
+    }
+    if (win == 0) {
+      return true;
+    }
+  }
+  return false;
+}
 
+bool checkColWin(char player, char board[3][3]) {
+  int win = 0;
+  for (int a = 0; a < 3; a++) {
+    win = 0;
+    for (int b = 0; b < 3; b++) {
+      if (board[a][b] != player) {
+	win++;
+      }
+    }
+    if (win == 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool checkDiagWin(char player, char board[3][3]) {
+  int win = 0;
+  int b = 0;
+  for (int a = 0; a < 3; a++) {
+    if (board[a][b] != player) {
+      win++;
+    }
+    b++;
+  }
+    if (win == 0) {
+      return true;
+    }
+    
+  win = 0;
+  b = 0;
+  for (int a = 3; a > 0; a--) {
+    if (board[a][b] != player) {
+      win++;
+    }
+    b++;
+  }
+    if (win == 0) {
+      return true;
+    }
+    
+  return false;
+}
+
+bool checkTie(char board[3][3]) {
+  int tie = 0;
+  for (int a = 0; a < 3; a++) {
+    for (int b = 0; b < 3; b++) {
+      if (board[a][b] != '-') {
+	tie++;
+      }
+    }
+    if (tie == 9) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool checkWin(char player, char board[3][3]) {
+  if (checkColWin(player, board) == true || checkRowWin(player, board) == true || checkDiagWin(player, board) == true) {
+    cout << player << " wins!" << endl;
+    return true;
+  }
+  return false;
+}
